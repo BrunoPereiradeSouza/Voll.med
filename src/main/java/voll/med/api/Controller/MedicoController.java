cf9341e8885966f7f016476625bdab50118016ac
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2025-05-23 19:04:53 -0300

    feat: create MedicoRepository

--- src/main/java/voll/med/api/Controller/MedicoController.java
+++ src/main/java/voll/med/api/Controller/MedicoController.java
@@ -1,15 +1,21 @@
 package voll.med.api.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import voll.med.api.medico.DadosCadastroMedico;
+import voll.med.api.medico.Medico;
+import voll.med.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
