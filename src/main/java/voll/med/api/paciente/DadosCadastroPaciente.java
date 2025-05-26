2c793abfc263e54f942961b3559589c6b4161f51
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2025-05-26 07:44:47 -0300

    feat: create Paciente entity

--- src/main/java/voll/med/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/voll/med/api/paciente/DadosCadastroPaciente.java
@@ -1,7 +1,9 @@
 package voll.med.api.paciente;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import voll.med.api.endereco.DadosEndereco;
 
@@ -17,5 +19,7 @@ public record DadosCadastroPaciente(
         @NotBlank
         @Pattern(regexp = "\\d{11}")
         String cpf,
+        @NotNull
+        @Valid
         DadosEndereco endereco) {
 }
