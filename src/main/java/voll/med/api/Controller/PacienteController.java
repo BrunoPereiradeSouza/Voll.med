e16aba2299eef60362a9c23193ae10a2a26363d0
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2025-05-26 07:35:57 -0300

    feat: validate DadosCadastroPaciente with Bean validation

--- src/main/java/voll/med/api/Controller/PacienteController.java
+++ src/main/java/voll/med/api/Controller/PacienteController.java
@@ -1,6 +1,7 @@
 package voll.med.api.Controller;
 
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import voll.med.api.paciente.DadosCadastroPaciente;
 public class PacienteController {
 
     @PostMapping
-    public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dados) {
+    public void cadastrarPaciente(@RequestBody @Valid DadosCadastroPaciente dados) {
         System.out.println(dados);
     }
 }
--- src/main/java/voll/med/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/voll/med/api/paciente/DadosCadastroPaciente.java
@@ -1,6 +1,21 @@
 package voll.med.api.paciente;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import voll.med.api.endereco.DadosEndereco;
 
-public record DadosCadastroPaciente(String nomeCompleto, String cpf, String email, String telefone, DadosEndereco endereco) {
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String cpf,
+        DadosEndereco endereco) {
 }
