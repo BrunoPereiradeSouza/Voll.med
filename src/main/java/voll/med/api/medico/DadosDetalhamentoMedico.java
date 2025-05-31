f220d0d33105cd096950254d521dbc6927ef4be3
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2025-05-31 10:43:39 -0300

    feat: add ResponseEntity return in medicos CRUD

---
+++ src/main/java/voll/med/api/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,11 @@
+package voll.med.api.medico;
+
+import voll.med.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhamentoMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
+
+    }
+}
